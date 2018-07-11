28376d4b706ded994fe7a155f87c63da606d3eea
wyx <3039298329@qq.com>
2018-07-11 16:37:20 +0800

    第一次提交

---
+++ src/main/java/com/baizhi/testApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Created by wyx on 2018/7/11.
+ */
+@SpringBootApplication
+public class testApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(testApplication.class, args);
+    }
+}
