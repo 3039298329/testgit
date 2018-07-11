d2baa49f9bf4f9ae32a9243c928ac58c2d35c0d4
wyx <3039298329@qq.com>
2018-07-11 17:04:52 +0800

    在分支中定义的方法

---
+++ src/main/java/com/baizhi/Controller/UserController.java
@@ -0,0 +1,19 @@
+package com.baizhi.Controller;
+
+import com.baizhi.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import javax.annotation.Resource;
+import javax.annotation.Resources;
+
+/**
+ * Created by wyx on 2018/7/11.
+ */
+@Controller
+public class UserController {
+    public static void main(String[] args) {
+        System.out.println("这是一个分支的测试，感受知识的神奇");
+    }
+
+}
